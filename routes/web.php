a26b69573273f02cc236fc45d140e368f83b4c51
PaulDenis <d.paul@outlook.it>
2021-07-23 17:32:47 +0200

    Creato scaffolding, register, login, migrate

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function() {
+
+        Route::get('/', 'HomeController@index')->name('home');
+});
+
+
+Route::get('/', 'HomeController@index')->name('home');
